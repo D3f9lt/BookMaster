fe783e7666459a7dbb0609bead13da125c9669c7
Denis Ivanushkin <Divanushkin7@outlook.com>
2024-03-13 13:20:00 +0300

    Добавил папки, создал BrowseCatalogWindow

--- BookMaster/View/Windows/BrowseCatalogWindow.xaml.cs
+++ BookMaster/View/Windows/BrowseCatalogWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BookMaster
+namespace BookMaster.View.Windows
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для BrowseCatalogWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class BrowseCatalogWindow : Window
     {
-        public MainWindow()
+        public BrowseCatalogWindow()
         {
             InitializeComponent();
         }
