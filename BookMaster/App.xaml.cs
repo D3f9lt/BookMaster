48996119253f0b2780cb3a4ca447b3eb1ca971e7
Denis Ivanushkin <Divanushkin7@outlook.com>
2024-03-27 13:56:23 +0300

    Много чего добавил

--- BookMaster/App.xaml.cs
+++ BookMaster/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookMaster.Model;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,7 @@ namespace BookMaster
     /// </summary>
     public partial class App : Application
     {
+        // Контекст данных
+        public static BookMasterEntities context = new BookMasterEntities();
     }
 }
